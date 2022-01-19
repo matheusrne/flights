c50a577e613be29a34ad696cf29dcd2a39893032
matheus nascimento <matheus.nascimento@boxdelivery.com.br>
2022-01-19 14:52:16 -0300

    refactor

--- app/Services/FlightsService.php
+++ app/Services/FlightsService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Http;
+use phpDocumentor\Reflection\Types\Collection;
 
 class FlightsService extends Service
 {
@@ -21,42 +22,20 @@ class FlightsService extends Service
      *
      * @return array
      */
-    public function list() : array
+    public function list(): array
     {
-        $flights = Http::get($this->baseUrl . 'flights')->json();
-        $flights = collect($flights);
+        $flights = $this->getFlights();
         $fares = $flights->groupBy('fare');
 
-        $data = [];
         $groups = [];
+        $data = [];
 
         foreach ($fares as $k => $fare) {
-            foreach ($fare as $flight) {
-                if ($flight['outbound'] == 1) {
-                    $data[$k]['outbound'][$flight['price']][] = $flight;
-                } else {
-                    $data[$k]['inbound'][$flight['price']][] = $flight;
-                }
-            }
-
-            foreach ($data[$k]['outbound'] as $v1 => $outbound) {
-                foreach ($data[$k]['inbound'] as $v2 => $inbound) {
-                    $groups[] = [
-                        'uniqueId' => md5($k.$v1.$v2),
-                        'totalPrice' => $v1 + $v2,
-                        'outbound' => $outbound,
-                        'inbound' => $inbound,
-                    ];
-                }
-            }
-
+            $data[$k] = $this->arrangeByType($fare);
+            $groups = array_merge($groups, $this->setGroups($data, $k));
         }
 
-        usort($groups, function ($item1, $item2) {
-            return $item1['totalPrice'] > $item2['totalPrice'];
-        });
-
-        $groups = collect($groups);
+        $groups = $this->sortGroups($groups);
 
         return [
             'flights' => $flights,
@@ -64,8 +43,76 @@ class FlightsService extends Service
             'totalGroups' => $groups->count(),
             'totalFlights' => $flights->count(),
             'cheapestPrice' => $groups[0]['totalPrice'],
-            'cheapestGroup' =>  $groups[0]['uniqueId'],
+            'cheapestGroup' => $groups[0]['uniqueId'],
         ];
+
+    }
+
+    /**
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    private function getFlights(): \Illuminate\Support\Collection
+    {
+        $flights = Http::get($this->baseUrl . 'flights')->json();
+        return collect($flights);
+    }
+
+    /**
+     *
+     * @param $fare
+     * @return array
+     */
+    private function arrangeByType($fare): array
+    {
+        $data = [];
+
+        foreach ($fare as $flight) {
+            if ($flight['outbound'] == 1) {
+                $data['outbound'][$flight['price']][] = $flight;
+            } else {
+                $data['inbound'][$flight['price']][] = $flight;
+            }
+        }
+        return $data;
+    }
+
+    /**
+     *
+     * @param $fare
+     * @return array
+     */
+    private function setGroups($data, $k): array
+    {
+        $groups = [];
+
+        foreach ($data[$k]['outbound'] as $v1 => $outbound) {
+            foreach ($data[$k]['inbound'] as $v2 => $inbound) {
+                $groups[] = [
+                    'uniqueId' => md5($k . $v1 . $v2),
+                    'totalPrice' => $v1 + $v2,
+                    'outbound' => $outbound,
+                    'inbound' => $inbound,
+                ];
+            }
+        }
+
+        return $groups;
+
+    }
+
+    /**
+     *
+     * @param $groups
+     * @return \Illuminate\Support\Collection
+     */
+    private function sortGroups($groups): \Illuminate\Support\Collection
+    {
+        usort($groups, function ($item1, $item2) {
+            return $item1['totalPrice'] > $item2['totalPrice'];
+        });
+
+        return collect($groups);
 
     }
 
